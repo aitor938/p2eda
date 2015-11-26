232d77ec7c72fd0b71efd0c23513b0ac978ce711
Alejandro Silva <alexsilva792@gmail.com>
2015-11-26 16:49:18 +0100

    * Subida inicial del proyecto

---
+++ src/Coleccion.java
@@ -0,0 +1,17 @@
+//Jose Luis Varo Guzmán
+
+public interface Coleccion<Id extends Comparable<Id>, In> {
+	//COMPLETAR
+	
+	
+	
+	/**
+	 * Devuelve un String con la información de todos los pares (identificador, información)
+	 * almacenados en la colección, separando los pares entre sí
+	 * con saltos de línea.
+	 * @return String con la información de todos los pares (identificador, información).
+	 */
+	public String listar();
+	
+
+}
