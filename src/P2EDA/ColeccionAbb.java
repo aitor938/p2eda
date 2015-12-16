9a359887dce0bbf414885e5777b07fd079270fc8
Aitor <aitor.odt.93@gmail.com>
2015-12-16 18:06:45 +0100

    *Modificacion metodo meter

--- src/P2EDA/ColeccionAbb.java
+++ src/P2EDA/ColeccionAbb.java
@@ -2,28 +2,43 @@ package P2EDA;
 
 public class ColeccionAbb<Id extends Comparable<Id>, In> implements Coleccion<Id, In> {
 
-    private NodoArbol<Id,In> raiz;
+    private NodoArbol<Id, In> raiz;
     private int size;
-    private NodoArbol<Id,In> iterador;
-    
+    private NodoArbol<Id, In> iterador;
+
     @Override
     public boolean meter(Id identificador, In informacion) {
-        NodoArbol<Id,In> elemento = new NodoArbol<>(identificador, informacion);
-        if (raiz == null){
+        NodoArbol<Id, In> elemento = new NodoArbol<>(identificador, informacion);
+        if (raiz == null) {
             raiz = elemento;
+            size++; //contador de elementos
+            return true;
+        } else if (raiz.esMayor(elemento)) {
+            return meterRec(raiz.getIzquierda(), elemento, raiz, false);
         } else {
-            if (raiz.esMayor(elemento)){
-                meterRec(raiz.getDerecha(),elemento,raiz);
-            } else {
-                
-            }
+            return meterRec(raiz.getDerecha(), elemento, raiz, true);
         }
-        return false;
     }
-    
-    private boolean meterRec(NodoArbol<Id,In> actual, NodoArbol<Id,In> elemento,NodoArbol<Id,In> anterior){ //el que era el actial ahora es el anterior
-        
-        return false;
+
+    //el booleano es para cuando lleguemos a nil saber que camino es 
+    //sin necesidad de volver a comparar el anterior con el elemento 
+    //recorrido; el que era el actial ahora es el anterior
+    private boolean meterRec(NodoArbol<Id, In> actual,
+            NodoArbol<Id, In> elemento, NodoArbol<Id, In> anterior, boolean esMayor) {
+        if (actual == null) {
+            if (esMayor) {
+                anterior.setDerecha(elemento);
+            } else {
+                anterior.setIzquierda(elemento);
+            }
+            size++;
+            return true;
+        } else if (actual.esMayor(elemento)) {
+            return meterRec(actual.getIzquierda(), elemento, actual, false);
+        } else {
+
+            return meterRec(actual.getDerecha(), elemento, actual, true);
+        }
     }
 
     @Override
