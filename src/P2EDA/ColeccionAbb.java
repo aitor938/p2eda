1e6d157f8b3334d98670623c762ad595af9e4166
jlvaro <jlvg190896@gmail.com>
2015-12-17 14:58:21 +0100

    Extiende BSTree, "esta?" implementado
    Extiende BSTree con Tupla como su tipo Elemento, implementado "esta?"; reformateado, eliminado código innecesario de netbeans/eclipse, comentado código que requiere ser modificado para usar BSTree o que lanza excepciones al compilar por falta de implementación, añadido código comentado del anterior "listar".

--- src/P2EDA/ColeccionAbb.java
+++ src/P2EDA/ColeccionAbb.java
@@ -1,14 +1,13 @@
-package P2EDA;
+public class ColeccionAbb<Id extends Comparable<Id>, In>extends
+ BSTree<Tupla<Id,In>> implements Coleccion<Id, In> {
 
-public class ColeccionAbb<Id extends Comparable<Id>, In> implements Coleccion<Id, In> {
-
-    private NodoArbol<Id, In> raiz;
+    //private NodoArbol<Id, In> raiz;
     private int size;
-    private NodoArbol<Id, In> iterador;
+    //private NodoArbol<Id, In> iterador;
 
-    @Override
     public boolean meter(Id identificador, In informacion) {
-        NodoArbol<Id, In> elemento = new NodoArbol<>(identificador, informacion);
+        //Modificar para BSTree
+        /*NodoArbol<Id, In> elemento = new NodoArbol<>(identificador, informacion);
         if (raiz == null) {
             raiz = elemento;
             size++; //contador de elementos
@@ -17,13 +16,16 @@ public class ColeccionAbb<Id extends Comparable<Id>, In> implements Coleccion<Id
             return meterRec(raiz.getIzquierda(), elemento, raiz, false);
         } else {
             return meterRec(raiz.getDerecha(), elemento, raiz, true);
-        }
+        }*/
+        return true;
     }
 
+    
+    //Modificar para BSTree
     //el booleano es para cuando lleguemos a nil saber que camino es 
     //sin necesidad de volver a comparar el anterior con el elemento 
     //recorrido; el que era el actial ahora es el anterior
-    private boolean meterRec(NodoArbol<Id, In> actual,
+    /*private boolean meterRec(NodoArbol<Id, In> actual,
             NodoArbol<Id, In> elemento, NodoArbol<Id, In> anterior, boolean esMayor) {
         if (actual == null) {
             if (esMayor) {
@@ -39,51 +41,51 @@ public class ColeccionAbb<Id extends Comparable<Id>, In> implements Coleccion<Id
 
             return meterRec(actual.getDerecha(), elemento, actual, true);
         }
-    }
+    }*/
 
-    @Override
     public boolean estaElemento(Id identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Compueba la existencia de la Tupla mediante una Tupla "dummy"
+		return isThere(new Tupla<Id,In>(identificador, null));
     }
+    
+    //!!IMPORTANTE!!//
+    //public In obtenerInformacion(Id identificador) {}
 
-    @Override
-    public In obtenerInformacion(Id identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
     public boolean borrar(Id identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //WIP
+        return true;
     }
 
-    @Override
     public int size() {
         return size;
     }
 
-    @Override
     public boolean esVacia() {
-        return raiz == null;
+        //WIP
+        return true;
     }
 
-    @Override
-    public String listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String listar() {String listado = "";
+	/*
+		iniciarIterador();
+		while (haySiguiente()) {
+			try {
+				siguiente();
+				listado = listado + elemento.toString() + "\n";
+			} catch (NoHaySiguienteException e) {}
+		}
+	*/
+		return listado;
+	}
     }
 
-    @Override
-    public void iniciarIterador() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    //Necesario?
+    //public void iniciarIterador() {}
 
-    @Override
     public boolean existeSiguiente() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //WIP
+        return true;
     }
 
-    @Override
-    public Tupla<Id, In> siguiente() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    //public Tupla<Id, In> siguiente() {}
 }
