b52d8ab7667ede2e28cd15dddaba7a0028b15b79
jlvaro <jlvg190896@gmail.com>
2015-12-17 14:45:40 +0100

    Tupla es Comparable, genérico
    Modificación hacia la correcta genereicidad (Es eso una palabra?) de Tupla, implementa Comparable; reformateado, eliminado código innecesario de netbeans/eclipse.

--- src/P2EDA/Tupla.java
+++ src/P2EDA/Tupla.java
@@ -1,31 +1,32 @@
-package P2EDA;
-
-
-public class Tupla<Id extends Comparable<Id>, In> {
-    private Id identificador;
-    private In informacion;
-
-    public Tupla() {
-    }
-
-    public Tupla(Id identificador, In informacion) {
-        this.identificador = identificador;
-        this.informacion = informacion;
-    }
-
-    public Id getIdentificador() {
-        return identificador;
-    }
-
-    public void setIdentificador(Id identificador) {
-        this.identificador = identificador;
-    }
-
-    public In getInformacion() {
-        return informacion;
-    }
-
-    public void setInformacion(In informacion) {
-        this.informacion = informacion;
-    }
+public class Tupla<Id extends Comparable<Id>, In> implements Comparable<Tupla<Id,In>>{
+	private Id identificador;
+	private In informacion;
+	
+	public Tupla() {}
+	
+	public Tupla(Id identificador, In informacion) {
+		this.identificador = identificador;
+		this.informacion = informacion;
+	}
+	
+	public Id getIdentificador() {
+		return identificador;
+	}
+	
+	public void setIdentificador(Id identificador) {
+		this.identificador = identificador;
+	}
+	
+	public In getInformacion() {
+		return informacion;
+	}
+	
+	public void setInformacion(In informacion) {
+		this.informacion = informacion;
+	}
+	
+	
+	public int compareTo(Tupla<Id,In> tupla){
+		return getIdentificador().compareTo(tupla.getIdentificador());
+	}
 }
